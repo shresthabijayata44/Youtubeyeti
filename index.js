9669d09530263435304be1b8aeb91af55f19e326
shresthabijayata44 <shresthabijayata44@gmail.com>
2024-03-22 18:16:18 -0400

    Add new code in git

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const getJokes = async() =>{
         const res = await fetch("https://api.chucknorris.io/jokes/random");
         const data = await res.json();
         const myJoke = document.querySelector('#myJoke');
+         myJoke = data.value;
    
         myJoke.innerHTML = data.value;
     }catch(error){
@@ -13,3 +14,29 @@ const getJokes = async() =>{
 window.addEventListener("load", ()=>{
     getJokes();
 })
+
+
+
+// function extractSSDI() {
+//     const elements = document.querySelectorAll('script[type="application/ld+json"]');
+//     for (const el of elements) {
+//       try {
+//         const parsedData = JSON.parse(el.textContent.trim());
+//         if (parsedData['@type'] === 'Person') {
+//           console.log('SSDI JSON Object:', parsedData);
+          
+//           break; 
+//         }
+//       } catch (error) {
+      
+//         console.error('Error parsing JSON:', error);
+//       }
+//     }
+//   }
+  
+
+// //   window.addEventListener('DOMContentLoaded', extractSSDI);
+
+// window.addEventListener("load", ()=>{
+//     extractSSDI();
+// })
